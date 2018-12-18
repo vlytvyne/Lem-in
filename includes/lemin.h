6cd6f5f548bed1e11457d59f982e2ccac71cd774
Vadym Lytvynets <v.lytvynets61@gmail.com>
2018-12-18 18:55:26 +0200

    structure

--- includes/lemin.h
+++ includes/lemin.h
@@ -10,4 +10,35 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef LEMIN_H
+# define LEMIN_H
 
+#include "libft.h"
+#include "printf.h"
+#include "get_next_line.h"
+
+typedef enum		e_sp_meaning
+{
+	NO_SPEC,
+	START,
+	END
+}					sp_mean;
+
+typedef struct		s_neighbors
+{
+	t_room			*room;
+	t_room			*next;
+}					t_neighbors;
+
+typedef	struct		s_room
+{
+	char			*name;
+	t_coor			coor;
+	sp_mean			sp_mean;
+	int				distance;
+//	int				ants_wait;
+	t_neighbors		*descendants;
+	struct s_room	*ancestor;
+}					t_room;
+
+#endif
